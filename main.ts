29e6d9dd32ccb7fb2ad5172fe9af9287d73828a1
Forthtilliath <Forth@live.fr>
2021-07-29 20:52:23 +0200

    Test

--- main.ts
+++ main.ts
@@ -2,6 +2,15 @@ import { Logger } from './log';
 
 Logger.log('TypeScript works!') ;
 
+// détermine qui commence 
 let player:Number = Math.floor(Math.random()*2);
 
-Logger.log(player);
+const nbCol:Number = 3;
+const nbLig:Number = 3;
+const nbCases:Number = nbLig * nbCol;
+
+let tabMorpion:Number[] = [... new Array(nbCases).keys()].map(x => -1);
+
+tabMorpion = Array.from(9, (_, i) =>-1) ; 
+
+Logger.log(player,tabMorpion);
